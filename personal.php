e431525bd58a26888fbe8843369a70fe3a390f69
Alexander Hornig <alexander@h-software.de>
2017-11-19 23:31:33 +0100

    fix controller routing on personal settings page

--- personal.php
+++ personal.php
@@ -3,7 +3,7 @@ use OCP\AppFramework\Http\TemplateResponse;
 
 // user settings
 $app = new \OCA\LdapContacts\AppInfo\Application();
-$settings = $app->getContainer()->query('UserSettingsController');
+$settings = $app->getContainer()->query('SettingsController');
 
 $property['order_by'] = $settings->getUserValue( 'order_by' );
 // generate template
